ff96e6a26d1a044c2dd6a9e80707635ae705c4da
leonardoPaiv <leopaiva45@gmail.com>
2022-06-11 23:28:17 -0300

    Code with tips about functions on buttons

--- Primeiro Projeto_Perguntas/project_questions/lib/main.dart
+++ Primeiro Projeto_Perguntas/project_questions/lib/main.dart
@@ -3,6 +3,16 @@ import 'package:flutter/material.dart';
 main() => runApp(new AskApp());
 
 class AskApp extends StatelessWidget {
+  void answer() {
+    print('question answer');
+  }
+
+  void Function() answer2() {
+    return () {
+      print('answer #2');
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
     final Questions = [
@@ -18,9 +28,21 @@ class AskApp extends StatelessWidget {
         body: Column(
           children: <Widget>[
             Text(Questions[0]),
-            ElevatedButton(child: Text('Resposta 01'), onPressed: null),
-            ElevatedButton(child: Text('Resposta 02'), onPressed: null),
-            ElevatedButton(child: Text('Resposta 03'), onPressed: null),
+            //                                         Chamando uma função que
+            //                                         não retorna um valor
+            ElevatedButton(child: Text('Resposta 01'), onPressed: answer),
+
+            // to call a function that return something, you can call a
+            // void function thats return other function
+            ElevatedButton(child: Text('Resposta 02'), onPressed: answer2()),
+            ElevatedButton(
+                child: Text('Resposta 03'),
+                onPressed: () {
+                  print('Answer 3');
+                }),
+            ElevatedButton(
+                child: Text('Resposta 04'),
+                onPressed: () => {print('Answer 4')}),
           ],
         ),
       ),
